5c7943d80ca4ff6e61b312373387e85c19719c4c
entryone <a.i.kulinich@gmail.com>
2018-12-17 18:04:06 +0200

    decorators transpiling in tests

--- packages/react-scripts/config/jest/babelTransform.js
+++ packages/react-scripts/config/jest/babelTransform.js
@@ -11,5 +11,6 @@ const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
   presets: [require.resolve('babel-preset-react-app')],
+  plugins: ["transform-decorators-legacy", "transform-class-properties"],
   babelrc: false,
 });
